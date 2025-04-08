28fe238fb21bf92546121267f6536bba03c128b1
jquizo <45918580+jquizo@users.noreply.github.com>
2025-04-08 20:52:19 +1000

    Added global css stylesheet
    global stylesheet for custom CSS as needed

--- app/root.tsx
+++ app/root.tsx
@@ -12,19 +12,19 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"; // Import the Footer component
 import ErrorBoundaryContent from "~/components/ErrorBoundaryContent";
 
+// CSS IMPORTS
 import "./tailwind.css";
+import globalstyles from './globalstyles.css?url';
 
 export const links: LinksFunction = () => [
+  // Preconnect
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  // Font imports
   {
     rel: "stylesheet",
     href: "https://fonts.cdnfonts.com/css/ocr-a-extended",
@@ -33,6 +33,12 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.cdnfonts.com/css/ethnocentric",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+  // Global stylesheet
+  { rel: "stylesheet", href: globalstyles },
 ];
 
 export const loader: LoaderFunction = async () => {
